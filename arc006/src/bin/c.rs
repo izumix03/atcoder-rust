ce59f7a0c971c21453c4a3e83082f86c9bc54e20
izumix03 <takahiro.izumikawa@gmail.com>
2022-05-27 10:36:42 +0900

    solve some problems

--- arc006/src/bin/c.rs
+++ arc006/src/bin/c.rs
@@ -3,25 +3,27 @@ use proconio::{input};
 fn main() {
     input! {
         n: usize,
-        mut w: [(i32);n]
+        mut w: [i32;n]
     }
 
     // 順序付き
     let mut boxes: Vec<i32> = Vec::new();
 
-    for (n) in w.iter() {
+    for n in w {
         boxes.sort();
         let mut ok = false;
         for (pos, b) in boxes.iter().enumerate() {
-            if *b > *n {
-                boxes[pos] = *n;
+            if *b >= n {
+                boxes[pos] = n;
                 ok = true;
                 break;
             }
         }
 
         if !ok {
-
+            boxes.push(n);
         }
     }
+
+    println!("{}", boxes.len());
 }
